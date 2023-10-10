607abf4e6b31afe855150944e6fff13913b339fa
Amer <amer.m.amora@gmail.com>
2023-10-11 01:05:42 +0300

    Remove build files from version control

--- DataLayer/Entities/AspNetRoleClaim.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-
-namespace DataLayer.Entities;
-
-public partial class AspNetRoleClaim : IdentityRoleClaim<Guid>
-{
-}
