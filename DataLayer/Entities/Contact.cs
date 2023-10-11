b4d5bce1941b79887a9f7bb812f0710ea5240663
YazeedAldamen <yazeed.aldamen@alweb.com>
2023-10-11 21:59:27 +0300

    Adding Entities: Package/ProviderServices/HotelRooms/Contact/Blog

---
+++ DataLayer/Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class Contact
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Message { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
