a5f3e355029e9528734f0cc070752cbe350f6ef7
Amer <amer.m.amora@gmail.com>
2023-10-17 09:40:46 +0300

    Adding Contact Us

---
+++ AdminDashboard/Models/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AdminDashboard.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Message { get; set; }
+
+    }
+}
