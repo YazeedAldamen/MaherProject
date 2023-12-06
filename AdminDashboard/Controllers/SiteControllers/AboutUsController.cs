f9ab70d6aec440a699ea9075f8e4a2dc5e344ea5
Amer <amer.m.amora@gmail.com>
2023-12-06 20:32:44 +0300

    Adding AboutUs

---
+++ AdminDashboard/Controllers/SiteControllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashboard.Controllers.SiteControllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
